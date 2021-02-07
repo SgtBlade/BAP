75b49260350c965a539876d0dccccb98a701976b
GilzenPilzen <gilles.van.biervliet@student.howest.be>
2021-02-07 23:56:39 +0100

    tabs have gotten styling

--- src/components/SimpleTabs/simpleTabs.js
+++ src/components/SimpleTabs/simpleTabs.js
@@ -1,79 +1,112 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-//import { makeStyles } from '@material-ui/core/styles';
+import {withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import ReactQuill from 'react-quill';
+import style from "./simpleTabs.module.css";
+
+const StyledTabs = withStyles({
+    root: {
+        borderBottom: '.5rem solid var(--durfYellow)'
+    },
+
+    indicator: {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+        height: '100%',
+        zIndex: '-1',
+        '& > span': {
+            width: '100%',
+            height: '100%',
+            borderRadius: '.5rem .5rem 0 0',
+            backgroundColor: '#ffed00',
+            zIndex: '-1',
+        },
+    },
+})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    fontFamily: "Montserrat_black",
+    fontSize: '1.6rem',
+    color: 'black',
+    '&:focus': {
+      opacity: 1,
+    },
+    
+  },
+}))((props) => <Tab disableRipple {...props} />);
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box className={style.tabContainer} p={3}>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
 }
-
+  
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
-export default function SimpleTabs(infProps){
-  //const classes = useStyles();
-  const [value, setValue] = useState(0);
+export default function CustomizedTabs(data) {
+  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event,newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <div>
-      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-        <Tab label="Project" {...a11yProps(0)} />
-        <Tab label="FAQ" {...a11yProps(1)} />
-        <Tab label="Updates" {...a11yProps(2)} />
-        <Tab label="Over ons" {...a11yProps(3)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        {infProps.content}
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-    </div>
+      <div className={style.tabs__container}>
+        <StyledTabs value={value} onChange={handleChange} aria-label="styled tabs example">
+          <StyledTab label="Workflows" {...a11yProps(0)}/>
+          <StyledTab label="Datasets" {...a11yProps(1)}/>
+          <StyledTab label="Connections" {...a11yProps(2)}/>
+          <StyledTab label="Connections" {...a11yProps(3)}/>
+        </StyledTabs>
+        <TabPanel value={value} index={0}>
+            <ReactQuill 
+                readOnly={true}
+                modules={{ "toolbar": false }}
+                value={data.description}
+                style={{height: '50rem', border:"1rem solid transparent", overflow:"hidden", marginTop: '2rem', marginBottom: '6rem'}}
+            />
+
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+            Item two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+            Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+            Item Four
+        </TabPanel>
+      </div>
   );
 }
-
-
-
-
-
-
-
