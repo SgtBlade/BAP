5ef245467f1e636b71d9d96c5d5ba7dda38aa6dc
aaronharinck <aaron.harinck@student.howest.be>
2021-02-09 00:39:10 +0100

    quiz child component to parrent communication

--- src/pages/Quiz/quiz.js
+++ src/pages/Quiz/quiz.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import { useStores } from "../../hooks/useStores";
 // import { useObserver } from "mobx-react-lite";
 
@@ -9,15 +9,28 @@ import CurrentStep from "./quizComponents/CurrentStep/currentStep";
 
 const style = { ...moduleStyle, ...globalStyle };
 
+// let curStepNum = 1;
+// const setCurStep = stepNum => {
+//   console.log(stepNum);
+// };
+
 const Quiz = () => {
+  const [stepNum, setCurStep] = useState(1);
+
+  const receivedStep = step => {
+    // receive nextStep from child component
+    setCurStep(step);
+    console.log(step);
+  };
+
   return (
     <div className={style.container}>
       <h1 className="hidden">Durf2030 - Quiz</h1>
       <h2 className={style.title}>
         Krijg jouw gerpersonaliseerde aanbevelingen
       </h2>
-      {/* progressbarcomponent */}
-      <CurrentStep></CurrentStep>
+      {/* send function reference to child component*/}
+      <CurrentStep onSetStep={receivedStep} currentStep={stepNum}></CurrentStep>
       {/* previous & next button */}
     </div>
   );
