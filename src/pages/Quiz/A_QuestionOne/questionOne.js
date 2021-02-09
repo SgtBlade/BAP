91ec6e30b01dd86b4ea141f9eca6ce9a9cbab633
aaronharinck <aaron.harinck@student.howest.be>
2021-02-09 22:20:58 +0100

    question one styling

--- src/pages/Quiz/A_QuestionOne/questionOne.js
+++ src/pages/Quiz/A_QuestionOne/questionOne.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./questionOne.module.css";
 
-const questionOne = () => {
+const QuestionOne = () => {
+  const [selectedAnswer, setSelectedAnswer] = useState();
+
   return (
     <div>
-      <p>QuestionOne</p>
+      <p className={style.questionTitle}>
+        Hoe sportief ben je in je vrije tijd?
+      </p>
+      <div className={style.answers}>
+        <button
+          type="button"
+          onClick={() => setSelectedAnswer(1)}
+          className={`${style.quizButton} ${style.quizButtonGrey} ${
+            selectedAnswer === 1 ? style.active : ""
+          }`}
+        >
+          <img
+            src="assets/images/quiz/nothingmeter.svg"
+            width="114"
+            height="114"
+            alt="Lage/lege actiefmeter"
+          ></img>
+          <span className={style.quizButtonText}>Niet sportief</span>
+        </button>
+        <button
+          type="button"
+          onClick={() => setSelectedAnswer(2)}
+          className={`${style.quizButton} ${style.quizButtonOrange}
+          ${selectedAnswer === 2 ? `${style.active}` : ""}`}
+        >
+          <img
+            src="assets/images/quiz/somemeter.svg"
+            width="114"
+            height="114"
+            alt="redelijk lage actiefmeter"
+          ></img>
+          <span className={style.quizButtonText}>Soms sportief</span>
+        </button>
+        <button
+          type="button"
+          onClick={() => setSelectedAnswer(3)}
+          className={`${style.quizButton} ${style.quizButtonGreen}
+          ${selectedAnswer === 3 ? `${style.active}` : ""}`}
+        >
+          <img
+            src="assets/images/quiz/regularmeter.svg"
+            width="114"
+            height="114"
+            alt="normale actiefmeter"
+          ></img>
+          <span className={style.quizButtonText}>Sportief</span>
+        </button>
+        <button
+          type="button"
+          onClick={() => setSelectedAnswer(4)}
+          className={`${style.quizButton} ${style.quizButtonRed}
+          ${selectedAnswer === 4 ? `${style.active}` : ""}`}
+        >
+          <img
+            src="assets/images/quiz/verymeter.svg"
+            width="114"
+            height="114"
+            alt="Hoge actiefmeter"
+          ></img>
+          <span className={style.quizButtonText}>Heel sportief</span>
+        </button>
+      </div>
     </div>
   );
 };
 
-export default questionOne;
+export default QuestionOne;
--- src/pages/Quiz/quizComponents/CurrentStep/currentStep.js
+++ src/pages/Quiz/quizComponents/CurrentStep/currentStep.js
@@ -1,8 +1,37 @@
 import React, { useState } from "react";
 
 import ProgressBar from "../Progressbar/progressbar";
+import QuestionOne from "../../A_QuestionOne/questionOne";
+
+import style from "./currentStep.module.css";
 
 //temporary hardcoded value
+
+const swapStepComponent = currentStep => {
+  let stepComponent;
+  switch (currentStep) {
+    case 1:
+      stepComponent = <QuestionOne></QuestionOne>;
+      break;
+    case 2:
+      stepComponent = "QuestionTwo";
+      break;
+    case 3:
+      stepComponent = "QuestionThree";
+      break;
+    case 4:
+      stepComponent = "QuestionFour";
+      break;
+    case 5:
+      stepComponent = "Results";
+      break;
+    default:
+      stepComponent =
+        "Vragen laden... duurt het langer dan 1min? Laad dan de pagina opnieuw";
+      break;
+  }
+  return stepComponent;
+};
 
 const CurrentStep = ({ onSetStep }) => {
   const [curStep, setCurStep] = useState(1);
@@ -10,13 +39,22 @@ const CurrentStep = ({ onSetStep }) => {
   return (
     <>
       <ProgressBar currentStep={curStep}></ProgressBar>
-      <p>CurrentStep</p>
+
+      <div className={style.stepQuestionContainer}>
+        <p className={style.currentQuestionTitle}>
+          {curStep <= 4 ? `${`Vraag ${curStep} van 4`}` : ""}
+        </p>
+
+        {/* Load the correct step component */}
+        {swapStepComponent(curStep)}
+      </div>
+
       <button onClick={() => setCurStep(curStep + 1)}>Add</button>
       {/* currentvraagstap */}
       <button onClick={() => onSetStep(curStep)}>
         Log Call func is in parent
       </button>
-      {/* currentvraag */}
+
       {/* previous & next button */}
     </>
   );
