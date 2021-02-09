6ba6ab938ec407daecff13db8022100bc69e5202
aaronharinck <aaron.harinck@student.howest.be>
2021-02-09 19:08:06 +0100

    scroll to top fix

--- src/pages/F_CreateProjectForm/navButtons/navButtons.js
+++ src/pages/F_CreateProjectForm/navButtons/navButtons.js
@@ -1,39 +1,61 @@
-import React from 'react'
+import React from "react";
 import { useObserver } from "mobx-react-lite";
 import style from "./navButtons.module.css";
-import globalStyle from '../../globalStyles/main.module.css'
+import globalStyle from "../../globalStyles/main.module.css";
 
-const NavButtons = ({validate, currentStep, errors, STEPS}) => {
-  
+const NavButtons = ({ validate, currentStep, errors, STEPS }) => {
   return useObserver(() => (
     <>
       <ul className={style.errorList}>
-        {
-        errors.value ?
-        Object.keys(errors.value).map(function(key, index) {
-          return <li key={index}>{errors.value[key]}</li>
-        })
-        : ''
-        }
+        {errors.value
+          ? Object.keys(errors.value).map(function (key, index) {
+              return <li key={index}>{errors.value[key]}</li>;
+            })
+          : ""}
       </ul>
       <div className={style.buttonWrapper}>
-      {currentStep.value !== 1 ? 
-      <p onClick={() => currentStep.func(currentStep.value-1)} className={`${globalStyle.mainButton} ${globalStyle.previousButton}`}>Terug naar vorige stap</p>: ''}
+        {currentStep.value !== 1 ? (
+          <p
+            onClick={() => currentStep.func(currentStep.value - 1)}
+            className={`${globalStyle.mainButton} ${globalStyle.previousButton}`}
+          >
+            Terug naar vorige stap
+          </p>
+        ) : (
+          ""
+        )}
 
-      {currentStep.value === 5 ?
-      <p onClick={async () => {
-        if(await validate())
-        {
-          currentStep.func(currentStep.value+1);window.scrollTo(0, 0);
-        }
-        }}  className={`${globalStyle.mainButton}`}>{STEPS[currentStep.value]}</p>
-      :
-      <p onClick={async () => {
-        if(await validate())
-        {
-          currentStep.func(currentStep.value+1);window.scrollTo(0, 0);}
-        }} className={`${globalStyle.mainButton} ${Object.keys(errors.value).length === 0 ? '' : globalStyle.disabledButton}`}>{STEPS[currentStep.value]}</p>
-      }
+        {currentStep.value === 5 ? (
+          <p
+            onClick={async () => {
+              if (await validate()) {
+                currentStep.func(currentStep.value + 1);
+                // because window.scrollTo does not work: use a querySelector on the root
+                document.querySelector("#root").scrollTop = 0;
+              }
+            }}
+            className={`${globalStyle.mainButton}`}
+          >
+            {STEPS[currentStep.value]}
+          </p>
+        ) : (
+          <p
+            onClick={async () => {
+              if (await validate()) {
+                currentStep.func(currentStep.value + 1);
+                // because window.scrollTo does not work: use a querySelector on the root
+                document.querySelector("#root").scrollTop = 0;
+              }
+            }}
+            className={`${globalStyle.mainButton} ${
+              Object.keys(errors.value).length === 0
+                ? ""
+                : globalStyle.disabledButton
+            }`}
+          >
+            {STEPS[currentStep.value]}
+          </p>
+        )}
       </div>
     </>
   ));
