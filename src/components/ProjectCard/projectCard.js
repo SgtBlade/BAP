8a494d122208fcde9e7ed52a5b059f07435657b2
Miguel De Pelsmaeker <migueleken@hotmail.com>
2021-02-11 23:18:03 +0100

    fixed an oopsi

--- src/components/ProjectCard/projectCard.js
+++ src/components/ProjectCard/projectCard.js
@@ -77,7 +77,7 @@ const ProjectCard = ({project}) => {
         }
         {project.deadline ?  getTime(project.deadlineDate) > 0 && !project.archived ? <p className={style.countDown}>{getTime(project.deadlineDate)} dagen resterend</p> : '' :'' }
         
-        <p className={`${style.preview} ${deadline ? style.previewMargin : ''}`}>
+        <p className={`${style.preview} ${project.deadline ? style.previewMargin : ''}`}>
           {project.previewText}
         </p>
   
