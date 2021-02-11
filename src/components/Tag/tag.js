453294cb9075b5e4ed1ddd7918b5185341e0a6dd
aaronharinck <aaron.harinck@student.howest.be>
2021-02-11 16:46:37 +0100

    prop key fix

--- src/components/Tag/tag.js
+++ src/components/Tag/tag.js
@@ -1,20 +1,25 @@
 import React from "react";
 import style from "./tag.module.css";
-import tagstyle from '../../pages/globalStyles/main.module.css'
+import tagstyle from "../../pages/globalStyles/main.module.css";
 
-const Tag = (props) => {
-
-    return (
-        <div key={props.key} className={style.tag}
-            style={{
-                margin: props.margin ? props.margin : "1rem"
-            }}
-        ><p className={style.tagContent}>
-            <span className={`${style.tagColor} ${tagstyle[props.text.replace('/', '').replace('+', '')]}`}
-                
-            ></span>{props.text}</p>
-        </div>
-    )
+const Tag = props => {
+  return (
+    <div
+      className={style.tag}
+      style={{
+        margin: props.margin ? props.margin : "1rem",
+      }}
+    >
+      <p className={style.tagContent}>
+        <span
+          className={`${style.tagColor} ${
+            tagstyle[props.text.replace("/", "").replace("+", "")]
+          }`}
+        ></span>
+        {props.text}
+      </p>
+    </div>
+  );
 };
 
 export default Tag;
